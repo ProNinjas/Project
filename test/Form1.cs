1873c89ffaf6282e414c6b9adc78e0989e09e409
Zhen <zhen446@gmail.com>
2016-09-27 18:54:38 +0100

    zhen

--- test/Form1.cs
+++ test/Form1.cs
@@ -16,5 +16,10 @@ namespace test
         {
             InitializeComponent();
         }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
